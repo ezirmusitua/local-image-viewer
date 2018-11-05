064a9caa8194647da8534e3900b5b8ca46152b25
ezirmusitua <jferroal@gmail.com>
2018-11-05 21:23:18 +0800

    Init server

---
+++ server/src/main.hmr.ts
@@ -0,0 +1,18 @@
+import {FastifyAdapter, NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+
+bootstrap();
---
+++ server/src/main.ts
@@ -0,0 +1,11 @@
+import {NestFactory, FastifyAdapter} from '@nestjs/core';
+import {ValidationPipe} from '@nestjs/common';
+import {AppModule} from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+
+bootstrap();
