492250c9a41fee204d773cfb9086175aa2b26dea
ezirmusitua <jferroal@gmail.com>
2018-12-31 00:32:42 +0800

    Feat: add gallery list component

---
+++ app/src/components/GalleryList.vue
@@ -0,0 +1,31 @@
+<style scoped lang="scss">
+  .gallery-list {
+    max-width: 1280px;
+
+    .card {
+      margin-bottom: 20px;
+    }
+  }
+
+</style>
+
+<template lang="pug">
+  v-container.gallery-list
+    v-layout(wrap)
+      v-flex(
+      v-for="n in 9"
+      :key="n"
+      )
+        GalleryCard.card
+
+</template>
+
+<script>
+  import { Component, Vue } from 'vue-property-decorator';
+  import GalleryCard from './GalleryCard';
+
+  @Component({
+    components: {GalleryCard},
+  })
+  export default class GalleryList extends Vue {}
+</script>
