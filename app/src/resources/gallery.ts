9b339332cd819b2f89926390ba517ebe1567c572
ezirmusitua <jferroal@gmail.com>
2018-12-31 12:53:48 +0800

    Fix: frontend gallery resource

--- app/src/resources/gallery.ts
+++ app/src/resources/gallery.ts
@@ -12,19 +12,23 @@ const Routes: RouteObject[] = [{
   method: METHODS.GET,
   name: GalleryAPIs.LIST,
   path: '',
-  paramsSchema: joi.object(),
-  dataSchema: joi.object(),
+  paramsSchema: {
+    name: joi.string().allow(['']),
+    pageIndex: joi.number().min(1).required(),
+    pageSize: joi.number().min(9).max(21).required(),
+  },
+  dataSchema: {},
 }, {
   method: METHODS.GET,
   name: GalleryAPIs.RANDOM,
   path: '/random',
-  paramsSchema: joi.object(),
-  dataSchema: joi.object(),
+  paramsSchema: {},
+  dataSchema: {},
 }];
 
 export const GalleryResource = new Resource(EndPoint, Routes)
 
-export function concatThumbnail(gallery: { id: number }) {
+export function concatThumbnail(gallery: { id?: number }) {
   const {id} = gallery;
   return `${Config.backend}/${EndPoint}/thumbnail?id=${id}`;
 }
