29e843af0e9b67ea12275a8c409459d660616ea2
ezirmusitua <jferroal@gmail.com>
2019-01-03 00:28:01 +0800

    Feat: add top breadcrumb

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,9 @@
-<style lang="scss">
-  html {
-    overflow-y: auto;
-  }
+<style scoped lang="scss">
+  @import './styles/color.scss';
 
   .container {
     padding: 0;
+    background-image: linear-gradient(120deg, $color-bg-grey__primary, $color-bg-grey__secondary);
 
     main {
       padding: 0;
@@ -13,10 +12,26 @@
 </style>
 
 <template lang="pug">
-  v-app
-    //v-toolbar(app)
+  v-app(dark)
+    v-toolbar(app)
+      v-breadcrumbs(large :items="navBreadcrumb" divider=">")
     v-container.container(fluid)
       keep-alive
         router-view(:key="$route.fullPath")
     //v-footer(app)
 </template>
+
+<script lang="ts">
+  import { Component, Vue, Watch } from 'vue-property-decorator';
+
+  @Component({})
+  export default class App extends Vue {
+    private navBreadcrumb = [];
+
+    @Watch('$route', {immediate: true, deep: true})
+    onRouteChanged(newRoute) {
+      this.navBreadcrumb = newRoute.meta.breadcrumb;
+    }
+  }
+</script>
+
--- app/src/router.ts
+++ app/src/router.ts
@@ -11,11 +11,30 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      meta: {
+        breadcrumb: [{
+          text: 'Home',
+          disabled: true,
+          href: '/',
+        }],
+      },
     },
     {
       path: '/gallery/:galleryId',
       name: 'GalleryViewer',
       component: Viewer,
+      meta: {
+        breadcrumb: [{
+          text: 'Home',
+          disabled: false,
+          href: '/',
+        }, {
+          text: 'Viewer',
+          disabled: true,
+          href: '/gallery/',
+        }],
+      },
+
     },
   ],
 });
